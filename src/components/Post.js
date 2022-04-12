4ac7a36e55ed09354eb2ae4c5c83c8b759e6159e
Patrick Meier <patrick.meier2@students.fhnw.ch>
2022-04-12 17:29:51 +0200

    added Proptypes and compose dev tools

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions/postActions'
 class Posts extends Component {
@@ -19,6 +20,11 @@ class Posts extends Component {
       </>
     )
   }
+}
+
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
 }
 
 const mapStateToProps = (state) => ({ posts: state.posts.items })
